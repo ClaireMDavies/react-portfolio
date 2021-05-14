d3e571d310af64375527621bdc2e9c609dc6f041
Claire Davies <claire.davies@junitech.com>
2021-05-14 20:31:32 +0100

    sorted footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./style.css";
+import twitter from "../../img/twitter.png";
+import facebook from "../../img/facebook.png";
+import linkedin from "../../img/linkedin.png";
+import github from "../../img/github.png";
+
 
 function Footer() {
   return (
+
+    
     <footer className="footer">
-      <span>Portfolio</span>
+      {/* <nav className="navbar fixed-bottom navbar-dark bg-success"> */}
+      <div className="container-fluid">
+          <nav className="navbar  navbar-dark bg-success">
+       <a  href="https://twitter.com/C_M_Davies" className="navbar-brand"> <img src={twitter}
+                    alt="twitter icon"/></a>
+            <a  href="https://www.facebook.com/claire.davies.7106" className="navbar-brand"> <img src={facebook}
+                    alt="facebook icon"/></a>
+            <a  href="https://www.linkedin.com/in/claire-davies-53835182/" className="navbar-brand" ><img
+                    src={linkedin} alt="linkedin icon"/></a>
+            <a  href="https://github.com/ClaireMDavies" className="navbar-brand" ><img src={github}
+                    alt="github icon"/></a>
+             </nav>       
+        </div>
+    {/* </nav> */}
+    <span className="copyright">© 2021 All Rights Reserved</span>
     </footer>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -17,7 +17,7 @@ function About() {
                                 <div className="row">
                               
                                     <div className="col-sm-12 col-md-4">
-                                    <img src={headshot} alt="HeadShot" class="img-fluid"  alt="smiling Claire"
+                                    <img src={headshot} alt="HeadShot" class="img-fluid"  
                                             id="headshot"/>
                                     </div>
                                     
