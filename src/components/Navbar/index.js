5f72bda3aad0a4c4f241971611d318ac7d48c90a
Claire Davies <claire.davies@junitech.com>
2021-05-14 13:51:11 +0100

    got links in navbar working

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Navbar() {
+
+    
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+            <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={
+                                window.location.pathname === "/" || window.location.pathname === "/about"
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/portfolio"
+                            className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            to="/contact"
+                            className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+                        >
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+
+            </div>
+            </div>
+        </nav>
+
+    );
+}
+
+export default Navbar;
