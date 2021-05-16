5cb7bbdbc982a59c309ebfe41af012351f022aa2
Claire Davies <claire.davies@junitech.com>
2021-05-16 23:08:06 +0100

    Applied HashRouter (again!)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
