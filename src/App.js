4cd38ad53eef9208a209f5824f3901d86021d774
Claire Davies <claire.davies@junitech.com>
2021-05-16 20:38:22 +0100

    changing router for githubpages deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -13,15 +13,14 @@ function App() {
         <Router>
             <div>
                 <Header />
-
                 <Wrapper>
-                   
-                    <Route exact path="/" component={About}  />
-                    <Route exact path="/about" component={About} />
-                    <Route exact path="/portfolio" component={Portfolio}  />
-                    <Route exact path="/contact" component={Contact}  />
+                    <Switch>
+                        <Route exact path="/" component={About} />
+                        <Route exact path="/about" component={About} />
+                        <Route exact path="/portfolio" component={Portfolio} />
+                        <Route exact path="/contact" component={Contact} />
+                    </Switch>
                 </Wrapper>
-
                 <Footer />
             </div>
         </Router>
