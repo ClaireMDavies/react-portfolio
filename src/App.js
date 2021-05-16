2fb69d274de518cf70104e5a0aac4de7f56c3b34
Claire Davies <claire.davies@junitech.com>
2021-05-16 22:42:09 +0100

    Attempting to get depkoyment working on Heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -14,12 +14,10 @@ function App() {
             <div>
                 <Header />
                 <Wrapper>
-                    <Switch>
-                        <Route exact path="/" component={About} />
-                        <Route exact path="/about" component={About} />
-                        <Route exact path="/portfolio" component={Portfolio} />
-                        <Route exact path="/contact" component={Contact} />
-                    </Switch>
+                    <Route exact path="/" component={About} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/portfolio" component={Portfolio} />
+                    <Route exact path="/contact" component={Contact} />
                 </Wrapper>
                 <Footer />
             </div>
